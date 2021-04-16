dde0a4e46af0e149b1556759359eb1e59182b62f
Joshua Riedel <josh.riedel98@gmail.com>
2021-04-16 18:49:11 -0500

    gets historical prices starting at start year up until yesterday

--- _1DataIn.py
+++ _1DataIn.py
@@ -6,28 +6,38 @@ from calendar import monthrange
 from collections import namedtuple
 import cryptocompare as cc
 
-def __main__():
-    # Load .env file and assign API key
-    load_dotenv()
-    apikey = os.getenv('CB_API_KEY')
+class DataIn:
+    def __init__(self, startYear):
+        eth_prices = []
+        EthSnip = namedtuple("EthSnip", ["year","month","day","price"])
+        # Get historical ETH prices from start year to end of last full year
+        for year in range(startYear, datetime.date.today().year):
+            for month in range(1,13):
+                for day in range(1, monthrange(startYear,month)[1] + 1):
+                    price = cc.get_historical_price('ETH', 'USD', datetime.datetime(year,month,day))
+                    eth_prices.append(EthSnip(year,month,day,price['ETH']['USD']))
+                print("Aquired prices for " + str(month) + "/" + str(year))
+            print("Aquired prices for " + str(year))
 
-    # Init API using key
-    cc.cryptocompare._set_api_key_parameter(apikey)\
+        # Get historical ETH prices from beginning of current year to last full month
+        year = datetime.date.today().year   
+        for month in range(1,datetime.date.today().month):
+            for day in range(1, monthrange(year,month)[1] + 1):
+                price = cc.get_historical_price('ETH', 'USD', datetime.datetime(year,month,day))
+                eth_prices.append(EthSnip(year,month,day,price['ETH']['USD']))
+            print("Aquired prices for " + str(month) + "/" + str(year))
 
-    # Init variables
-    day=1
-    month=1
-    year=2020
-    eth_prices=[]
-    EthSnip = namedtuple("EthSnip", ["year","month","day","price"])
-
-    # Get historical ETH prices for every day in 2020
-    for month in range(1,13):
-        for day in range(1, monthrange(year,month)[1] + 1):
+        # Get historical ETH prices of the current month up to the current day
+        startMonth = datetime.date.today().month
+        for day in range(1, datetime.date.today().day):
             price = cc.get_historical_price('ETH', 'USD', datetime.datetime(year,month,day))
-            eth_prices.append(EthSnip(year,month,day,price))
+            eth_prices.append(EthSnip(year,month,day,price['ETH']['USD']))
 
-    print(eth_prices)
 
+def __main__():
+    load_dotenv()
+    cc.cryptocompare._set_api_key_parameter(os.getenv('CB_API_KEY'))
+
+    data = DataIn(2020)
 
 __main__()
