863e9ae5db97e81131b8b4d1a57931366264089b
Joshua Riedel <josh.riedel98@gmail.com>
2021-04-16 16:05:04 -0500

    switched to cryptocompare api
    getting historical data from every day in 2020

--- _1DataIn.py
+++ _1DataIn.py
@@ -1,16 +1,33 @@
 import os
-import coinbase as cb
 import requests
 from dotenv import load_dotenv
+import datetime
+from calendar import monthrange
+from collections import namedtuple
+import cryptocompare as cc
 
 def __main__():
+    # Load .env file and assign API key
     load_dotenv()
     apikey = os.getenv('CB_API_KEY')
-    apisecret = os.getenv('CB_API_SECRET')
-    coinbase = cb.Coinbase.with_api_key(apikey, apisecret)
-    #user = coinbase.get_user()
-    #print(user["name"])
-    #balance = coinbase.get_balance()
-    #print("Balance: " + balance)
+
+    # Init API using key
+    cc.cryptocompare._set_api_key_parameter(apikey)\
+
+    # Init variables
+    day=1
+    month=1
+    year=2020
+    eth_prices=[]
+    EthSnip = namedtuple("EthSnip", ["year","month","day","price"])
+
+    # Get historical ETH prices for every day in 2020
+    for month in range(1,13):
+        for day in range(1, monthrange(year,month)[1] + 1):
+            price = cc.get_historical_price('ETH', 'USD', datetime.datetime(year,month,day))
+            eth_prices.append(EthSnip(year,month,day,price))
+
+    print(eth_prices)
+
 
 __main__()
