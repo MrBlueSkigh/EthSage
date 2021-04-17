f89ba2f48f207fcb52a32558d818dff0ca9fc0dd
Joshua Riedel <josh.riedel98@gmail.com>
2021-04-17 00:00:21 -0500

    Revert "removing mysql, sending data to json"
    This reverts commit fb3cd4baec445d9458da626bda87f3aadf8d0a10.

--- _2ProccessData.py
+++ _2ProccessData.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import numpy as np
-import json
 from _1DataIn import DataIn
 
 class ProccessData:
@@ -12,13 +11,7 @@ class ProccessData:
     
     def CreateDataframe(self, prices):
         ethdf = pd.DataFrame(prices, columns=['Year','Month','Day','Price'])
-        self.SendToJSON(ethdf)
-
-    def SendToJSON(self, df):
-        jsonString = json.dumps(json.loads(df.to_json(orient="index")),indent=4)
-        with open('EthPrices.json','x') as f:
-            f.write(jsonString)
-
+        print(ethdf)
 
 
 
