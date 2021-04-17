5c0c846a6818539b3a57e5499b2a30661a53ba38
Joshua Riedel <josh.riedel98@gmail.com>
2021-04-16 23:19:36 -0500

    creating file to process data into dataframe

---
+++ _2ProccessData.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import numpy as np
+from _1DataIn import DataIn
+
+class ProccessData:
+    def __init__(self):
+        di = DataIn()
+        di.QueryAPI()
+    
+    def CreateDataframe(self, pricelist):
+        print('df')
+
+
+
+
+def __main__():
+    p = ProccessData()
+
+__main__()
