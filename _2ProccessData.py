6fd5281fdf1d8f5d486850f125b99d1217b6c04d
Joshua Riedel <josh.riedel98@gmail.com>
2021-04-17 00:04:27 -0500

    removing mysql, sending data to json

--- _2ProccessData.py
+++ _2ProccessData.py
@@ -1,20 +1,23 @@
 import os
 import pandas as pd
 import numpy as np
+import json
 from _1DataIn import DataIn
 
 class ProccessData:
     def __init__(self):
         di = DataIn()
         prices = di.QueryAPI()
-        self.CreateDataframe(prices)
+        self.__CreateDataframe(prices)
     
-    def CreateDataframe(self, prices):
+    def __CreateDataframe(self, prices):
         ethdf = pd.DataFrame(prices, columns=['Year','Month','Day','Price'])
-        print(ethdf)
+        self.__SendToJson(ethdf)
 
-
-
+    def __SendToJson(self, df):
+        jsonString = json.dumps(json.loads(df.to_json(orient="index")), indent=4)
+        with open('EthPrices.json','x') as f:
+            f.write(jsonString)
 
 def __main__():
     p = ProccessData()
