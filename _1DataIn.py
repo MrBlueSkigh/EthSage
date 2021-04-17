1f4db2e00551235f888353da1aaddf4d77abd208
Joshua Riedel <josh.riedel98@gmail.com>
2021-04-16 21:21:44 -0500

    inserts elements into local db

--- _1DataIn.py
+++ _1DataIn.py
@@ -4,18 +4,23 @@ from dotenv import load_dotenv
 import datetime
 from calendar import monthrange
 from collections import namedtuple
+import mysql.connector
 import cryptocompare as cc
 
 class DataIn:
+    __ethPrices = []
+
     def __init__(self, startYear):
-        eth_prices = []
+       self.__QueryAPI(startYear)
+       
+    def __QueryAPI(self, startYear):
         EthSnip = namedtuple("EthSnip", ["year","month","day","price"])
         # Get historical ETH prices from start year to end of last full year
         for year in range(startYear, datetime.date.today().year):
             for month in range(1,13):
                 for day in range(1, monthrange(startYear,month)[1] + 1):
                     price = cc.get_historical_price('ETH', 'USD', datetime.datetime(year,month,day))
-                    eth_prices.append(EthSnip(year,month,day,price['ETH']['USD']))
+                    self.__ethPrices.append(EthSnip(year,month,day,price['ETH']['USD']))
                 print("Aquired prices for " + str(month) + "/" + str(year))
             print("Aquired prices for " + str(year))
 
@@ -24,20 +29,39 @@ class DataIn:
         for month in range(1,datetime.date.today().month):
             for day in range(1, monthrange(year,month)[1] + 1):
                 price = cc.get_historical_price('ETH', 'USD', datetime.datetime(year,month,day))
-                eth_prices.append(EthSnip(year,month,day,price['ETH']['USD']))
+                self.__ethPrices.append(EthSnip(year,month,day,price['ETH']['USD']))
             print("Aquired prices for " + str(month) + "/" + str(year))
 
         # Get historical ETH prices of the current month up to the current day
         startMonth = datetime.date.today().month
         for day in range(1, datetime.date.today().day):
             price = cc.get_historical_price('ETH', 'USD', datetime.datetime(year,month,day))
-            eth_prices.append(EthSnip(year,month,day,price['ETH']['USD']))
+            self.__ethPrices.append(EthSnip(year,month,day,price['ETH']['USD']))
+        
+        self.__InsertIntoDB(self.__ethPrices)
 
+    def __InsertIntoDB(self, prices):
+        # Establish DB Connection
+        db = mysql.connector.connect(
+            host="localhost",
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASS'),
+            database="ethsage",
+        )
+
+        # Insert each item into the DB
+        ethcursor = db.cursor()
+        for e in prices:
+            queryString='INSERT INTO ethprices(year,month,day,price) VALUES (%s,%s,%s,%s)'
+            values=(e[0], e[1], e[2], e[3])
+            ethcursor.execute(queryString, values)
+            db.commit()
 
 def __main__():
     load_dotenv()
     cc.cryptocompare._set_api_key_parameter(os.getenv('CB_API_KEY'))
 
-    data = DataIn(2020)
+    year = input("Start year: ")
+    data = DataIn(int(year))
 
 __main__()
