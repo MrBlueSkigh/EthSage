8d1ef65fcb6ecb35ca20697d42bab495cde58c7c
Joshua Riedel <josh.riedel98@gmail.com>
2021-04-27 12:30:45 -0500

    can now plot data on a line graph

--- _3PlotData.py
+++ _3PlotData.py
@@ -6,23 +6,12 @@ from _2ProccessData import ProccessData
 class PlotData():
     def __init__(self):
         df = ProccessData.GetDataFromJson(self, 1)
-        self.TestPlot(df)
-        
-    def __CreatePlot(self, df, height_multiplier, min_val, max_val):
-        image_width = len(df)
-        image_height = height_multiplier*image_width
-        image = np.zeros((image_height, image_width))
-        factor = image_height/(max_val-min_val)
-        plt.plot()
+        self.Plot(df)
 
-    def TestPlot(self, df):
-        start = 1
-        timestep = 50
-        window = int(timestep*1)
-        max_val = df[start-window:start+timestep].max()
-        min_val = df[start-window:start+timestep].min()
-        #image = self.__CreatePlot(data[start:start+timestep], 1, min_val, max_val)
-        plt.plot(df, df, 'r--')
+    def Plot(self, df):
+        plt.plot(df.columns, df.loc["Price"], 'r--')
+        plt.ylabel('Price')
+        plt.xlabel('Days since {day:.0f}/{month:.0f}/{year:.0f}'.format(day=df[0].Day, month=df[0].Month, year=df[0].Year))
         plt.show()
 
 
